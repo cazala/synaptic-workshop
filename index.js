261b3c6fb4ce46a05ec216b87616f83597c648d9
Juan Cazala <cazala@mail.sfsu.edu>
2017-02-13 14:08:40 -0800

    neuron

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 var synaptic = require('synaptic')
+
+var Neuron = synaptic.Neuron
+
+var A = new Neuron(2)
+var B = new Neuron(1)
+
+A.project(B)
+
+var rate = 0.1
+for (let i = 0; i < 5000; i++) {
+  // 0 => 1
+  A.activate(0)
+  B.activate()
+  B.propagate(rate, 1)
+  A.propagate(rate)
+}
+
+A.activate(0)
+console.log(B.activate())
